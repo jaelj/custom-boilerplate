bb58cfde3027cf5c50ff758b81bd847de1f30221
Jael Johnson <jajo6032@colorado.edu>
2020-03-29 17:02:19 -0600

    index updated with background pink and google font

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
-// const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-
 function style() {
 return gulp.src('./scss/**/*.scss')
-.pipe(sourcemaps.init())
 //default style indented based on nesting
 .pipe(sass({outputStyle: 'nested'}))
 //indented but not based on nesting
@@ -15,14 +11,9 @@ return gulp.src('./scss/**/*.scss')
 // .pipe(sass({outputStyle: 'compact'}))
 //production css (minified) one line no white space
 // .pipe(sass({outputStyle: 'compressed'}))
-.pipe(browserSync.stream())
-.pipe(autoprefixer({
-    cascade: false
-}))
-.pipe(sourcemaps.write('.'))
-.pipe(gulp.dest('./css'));
+.pipe(gulp.dest('./css'))
+.pipe(browserSync.stream());
 }
-
 function watch() { browserSync.init({
 server: {
 baseDir: './' }
@@ -31,7 +22,5 @@ gulp.watch('./scss/**/*.scss', {ignoreInitial: false}, style);
 gulp.watch('./*.html').on('change', browserSync.reload);
 gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
-
 exports.style = style;
 exports.watch = watch;
-
